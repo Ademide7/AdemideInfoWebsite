9dfbcb1db8c085116e8e8e70e12f09d444230774
Ibrahim Ademide <ibrahim.ademide@yahoo.com>
2026-06-26 14:08:51 +0100

    update

--- ademideinfowebsite.client/vite.config.js
+++ ademideinfowebsite.client/vite.config.js
@@ -1,67 +1,57 @@
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
+import child_process from "child_process";
+import { fileURLToPath, URL } from "node:url";
+import { env } from "process";
 
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import child_process from 'child_process';
-import { env } from 'process';
+const isVercel = !!process.env.VERCEL;
 
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
+let https = undefined;
 
-const certificateName = "ademideinfowebsite.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+if (!isVercel) {
+    const baseFolder =
+        env.APPDATA && env.APPDATA !== ""
+            ? `${env.APPDATA}/ASP.NET/https`
+            : `${env.HOME}/.aspnet/https`;
 
-if (!fs.existsSync(baseFolder)) {
-    fs.mkdirSync(baseFolder, { recursive: true });
-}
+    const certificateName = "ademideinfowebsite.client";
+    const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+    const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit' }).status) {
-        throw new Error("Could not create certificate.");
+    if (!fs.existsSync(baseFolder)) {
+        fs.mkdirSync(baseFolder, { recursive: true });
     }
-}
 
-const target = env.ASPNETCORE_HTTPS_PORT
-    ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
-    : env.ASPNETCORE_URLS
-        ? env.ASPNETCORE_URLS.split(';')[0]
-        : 'https://localhost:7151';
+    if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+        child_process.spawnSync("dotnet", [
+            "dev-certs",
+            "https",
+            "--export-path",
+            certFilePath,
+            "--format",
+            "Pem",
+            "--no-password",
+        ]);
+    }
+
+    https = {
+        key: fs.readFileSync(keyFilePath),
+        cert: fs.readFileSync(certFilePath),
+    };
+}
 
 export default defineConfig({
-    base: "/AdemideInfoWebsite/",   // <-- Add this
-
-    plugins: [plugin()],
-
+    base: "/AdemideInfoWebsite/",
+    plugins: [react()],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-
-    server: {
-        proxy: {
-            '^/weatherforecast': {
-                target,
-                secure: false
-            }
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
-        port: parseInt(env.DEV_SERVER_PORT || '64934'),
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
-        }
-    }
+    },
+    server: {
+        https,
+        port: 64934,
+    },
 });
